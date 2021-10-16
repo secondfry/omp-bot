8b0abb04a8cb85253e21f625ad8cd72adac26acc
Rustam Gubaydullin <secondfry@yandex-team.ru>
2021-10-16 21:16:17 +0300

    Fix theservice typo in help

--- internal/app/commands/insurance/theservice/command_help.go
+++ internal/app/commands/insurance/theservice/command_help.go
@@ -4,13 +4,13 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-var help = `/help__insurance__service — print list of commands
-/get__insurance__service — get a entity
-/list__insurance__service — get a list of your entity (💎: with pagination via telegram keyboard)
-/delete__insurance__service — delete an existing entity
+var help = `/help__insurance__theservice — print list of commands
+/get__insurance__theservice — get a entity
+/list__insurance__theservice — get a list of your entity (💎: with pagination via telegram keyboard)
+/delete__insurance__theservice — delete an existing entity
 
-/new__insurance__service — create a new entity // not implemented (💎: implement list fields via arguments)
-/edit__insurance__service — edit a entity      // not implemented`
+/new__insurance__theservice — create a new entity // not implemented (💎: implement list fields via arguments)
+/edit__insurance__theservice — edit a entity      // not implemented`
 
 func (c *InsuranceTheServiceCommander) Help(inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, help)
