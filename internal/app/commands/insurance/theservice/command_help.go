02d308f05d3f15305f37b7351fca06f616a4294a
Rustam Gubaydullin <secondfry@yandex-team.ru>
2021-10-17 17:51:57 +0300

    Update help message

--- internal/app/commands/insurance/theservice/command_help.go
+++ internal/app/commands/insurance/theservice/command_help.go
@@ -9,8 +9,8 @@ var help = `/help__insurance__theservice — print list of commands
 /list__insurance__theservice — get a list of entities with pagination
 /delete__insurance__theservice <idx> — delete an existing entity
 
-/new__insurance__theservice — create a new entity // not implemented (💎: implement list fields via arguments)
-/edit__insurance__theservice — edit a entity      // not implemented`
+/new__insurance__theservice <json> — create a new entity
+/edit__insurance__theservice <idx> <json> — edit an entity`
 
 func (c *InsuranceTheServiceCommander) Help(inputMessage *tgbotapi.Message) {
 	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, help)
