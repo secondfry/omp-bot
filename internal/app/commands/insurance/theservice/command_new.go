8be817aa9f13c0c7bf82a7c8dffa481efcc3c5a2
Rustam Gubaydullin <secondfry@yandex-team.ru>
2021-10-17 17:26:07 +0300

    Implement New for TheServiceCommander

--- internal/app/commands/insurance/theservice/command_new.go
+++ internal/app/commands/insurance/theservice/command_new.go
@@ -1,11 +1,30 @@
 package theservice
 
 import (
-	"errors"
+	"encoding/json"
+	"fmt"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	theservice "github.com/ozonmp/omp-bot/internal/service/insurance/theservice"
 )
 
-func (*InsuranceTheServiceCommander) New(*tgbotapi.Message) error {
-	return errors.New("not implemented")
+func (c *InsuranceTheServiceCommander) New(msg *tgbotapi.Message) error {
+	newservice := theservice.TheService{}
+
+	println(msg.Text)
+
+	err := json.Unmarshal([]byte(msg.Text), &newservice)
+	if err != nil {
+		c.bot.Send(tgbotapi.NewMessage(msg.Chat.ID, fmt.Sprintf("Couldn't parse JSON: %s", err)))
+		return err
+	}
+
+	idx, err := c.service.Create(newservice)
+	if err != nil {
+		c.bot.Send(tgbotapi.NewMessage(msg.Chat.ID, fmt.Sprintf("Couldn't create TheService: %s", err)))
+		return err
+	}
+
+	c.bot.Send(tgbotapi.NewMessage(msg.Chat.ID, fmt.Sprintf("Created #%d", idx)))
+	return nil
 }
